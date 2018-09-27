f16e80afaecbaffb153fc780f076ecfcfa6d1b12
Aisha Coles <coles_aisha@yahoo.com>
2018-09-27 16:51:48 -0700

    first commit. added first url test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+		it('all feeds are defined and have URL', function() {
+			for(let feed of allFeeds) {
+				//feed = this.url;
+				expect(feed.url).toBeDefined();
+				expect(feed.url).not.toBe(0);
+			}
+		});
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
